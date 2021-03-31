05b97aabaa2867b0077df23a6256dfd392199a28
JaceDreams <jacedreams@gmail.com>
2021-03-31 15:16:24 -0400

    Implemented Main Menu
    Added Main Menu
    
    Added Themes for Pyramid Solitaire
    
    Still Bugs to fix but I wanted to test to see if the site will work

---
+++ Assets/__Scripts/TitleScreen/MainMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayProspector ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void PlayWestern()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+    public void PlayBeach()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+    }
+
+    public void PlayCasino()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 4);
+    }
+
+
+    public void HomeButton (string __Main_Scene)
+    {
+        SceneManager.LoadScene(__Main_Scene);
+    }
+
+
+}
